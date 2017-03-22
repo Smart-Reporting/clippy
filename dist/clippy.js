64dabbe41ec84a3f5803adf7468a868071800d02
André Schmidt <email.andre@gmx.net>
2017-03-22 16:15:45 +0100

    Changed clippy to be amd module

--- dist/clippy.js
+++ dist/clippy.js
@@ -1,11 +1,4 @@
-(function (dependencies, factory) {
-    if (typeof module === 'object' && typeof module.exports === 'object') {
-        var v = factory(require, exports); if (v !== undefined) module.exports = v;
-    }
-    else if (typeof define === 'function' && define.amd) {
-        define(dependencies, factory);
-    }
-})(["require", "exports"], function (require, exports) {
+define(["require", "exports"], function (require, exports) {
     "use strict";
     var Clippy = (function () {
         function Clippy() {
